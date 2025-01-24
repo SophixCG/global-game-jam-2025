85fbcb90ca56710031b02dae45a6bd68e7767612
Sophix <SophixCG@gmail.com>
2025-01-23 21:24:03 -0600

    test-push

---
+++ Global Game Jam 2025/Assets/Scenes/Scripts/QuitGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            Application.Quit(); 
+        } 
+    }
+}
